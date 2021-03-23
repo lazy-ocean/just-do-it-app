5e8a5a557e25a873126373494614c6f5bce77147
Vladlena Panchenko <57011546+lazy-ocean@users.noreply.github.com>
2021-03-23 15:22:42 +0100

    feat: add header

--- app/src/App/components/Header/index.jsx
+++ app/src/App/components/Header/index.jsx
@@ -5,7 +5,7 @@ const Header = ({ children }) => {
   return (
     <header>
       <div className="header__txt">
-        <img src="../logo512.png" alt="ToDo logo" className="header__logo" />
+        {/*         <img src="../logo512.png" alt="ToDo logo" className="header__logo" /> */}
         <h3 className="header__h">Just Do It App</h3>
       </div>
       <div className="header__buttons">{children}</div>
--- app/src/App/components/buttons/index.jsx
+++ app/src/App/components/buttons/index.jsx
@@ -1,14 +1,23 @@
 import React from "react";
+import "./button.css";
 
 export const AuthButtonGroup = ({ manageModal }) => {
   return (
     <>
-      <Button onClick={() => manageModal("login")}>Login</Button>
-      <Button onClick={() => manageModal("register")}>Register</Button>
+      <Button classn="login" onClick={() => manageModal("login")}>
+        Login
+      </Button>
+      <Button classn="reg" onClick={() => manageModal("register")}>
+        Register
+      </Button>
     </>
   );
 };
 
-export const Button = ({ children, onClick }) => {
-  return <button onClick={onClick}>{children}</button>;
+export const Button = ({ children, onClick, classn }) => {
+  return (
+    <button onClick={onClick} className={`btn btn--${classn}`}>
+      {children}
+    </button>
+  );
 };
